e7e9210d0e5d250ec51aa5f5502d0a94db95168c
Taleem <taleemmankuer@gmail.com>
2023-06-11 13:01:21 +0300

    inital commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center">
+      <h1>My Store</h1>
+    </main>
+  );
+}
