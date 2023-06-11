54d208ca2e0a1eae426ef62c1d5b8805f8c6d05f
Taleem <taleemmankuer@gmail.com>
2023-06-11 18:16:27 +0300

    add image domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["fakestoreapi.com", "links.papareact.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
