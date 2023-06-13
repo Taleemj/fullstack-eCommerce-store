57dfbb184415ccbc8d05ee50e1bc968435a7abfb
Taleem <taleemmankuer@gmail.com>
2023-06-13 20:19:19 +0300

    update

--- components/ProductsFeed.jsx
+++ components/ProductsFeed.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 
 const ProductsFeed = ({ products }) => {
   return (
-    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-64">
       {products.slice(0, 8).map((product) => (
         <Product key={product.id} product={product} />
       ))}
