f11cc5b289e630c8eb171ce5069a60468d54ab18
Taleem <taleemmankuer@gmail.com>
2023-06-22 18:11:52 +0300

    update

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyBjuYaznkb15JtixrYa0hXIDrrhwuRzir0",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "shellstuff-a5f5a.firebaseapp.com",
   projectId: "shellstuff-a5f5a",
   storageBucket: "shellstuff-a5f5a.appspot.com",
   messagingSenderId: "638997906235",
-  appId: "1:638997906235:web:7a402df4e9847e7843ed1e",
+  appId: process.env.FIREBASE_APP_ID,
 };
